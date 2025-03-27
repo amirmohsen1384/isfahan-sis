ef079bafc2066f25506ef4867c335f9b5ff1962a
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-03-27 06:05:06 +0330

    Defined the student
    This provides the information about a student

---
+++ include/data/student.h
@@ -0,0 +1,44 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include "person.h"
+
+class Student : public Person
+{
+    Q_OBJECT
+public:
+    enum class State {Rejection, Probation, Normal, Honor}; Q_ENUM(State)
+
+public:
+    explicit Student(QObject *parent = nullptr);
+    Student(const Student &another, QObject *parent = nullptr);
+
+    Student& operator=(const Student &another);
+
+    friend QDataStream& operator<<(QDataStream &stream, const Student &data);
+    friend QDataStream& operator>>(QDataStream &stream, Student &data);
+
+    virtual void commitToRecord() const override;
+    static Student loadFromRecord(const Entity &value);
+
+    float getScore() const;
+    void setScore(float value);
+
+    Student::State getState() const;
+    
+    quint8 getMinimumCredits() const;
+    quint8 getMaximumCredits() const;
+
+    virtual void addCredit(Lesson &lesson) override;
+    virtual void removeCredit(Lesson &lesson) override;
+
+private:
+    float score = 0.00;
+};
+
+QDataStream& operator<<(QDataStream &stream, const Student &data);
+QDataStream& operator>>(QDataStream &stream, Student &data);
+
+Q_DECLARE_METATYPE(Student)
+
+#endif // STUDENT_H
---
+++ src/data/student.cpp
@@ -0,0 +1,140 @@
+#include "include/data/student.h"
+#include "include/errors/education.h"
+
+QString getStudentFileName(const Entity &value)
+{
+    QDir mainDirectory = Entity::getEntityDirectory();
+    mainDirectory.mkdir("Students");
+    mainDirectory.cd("Students");
+    return mainDirectory.absoluteFilePath(QString("%1.stf").arg(value.getIdentifier()));
+}
+
+Student::Student(QObject *parent) : Person{parent} {}
+
+Student::Student(const Student &another, QObject *parent) : Student{parent}
+{
+    *this = another;
+}
+
+Student& Student::operator=(const Student &another)
+{
+    this->score = another.score;
+    return *this;
+}
+
+void Student::commitToRecord() const
+{
+    QFile file(getStudentFileName(*this));
+    if(!file.open(QFile::WriteOnly)) {
+        return;
+    }
+    QDataStream stream(&file);
+    stream << *this;
+    return;
+}
+
+Student Student::loadFromRecord(const Entity &value)
+{
+    QFile file(getStudentFileName(value));
+    if(!file.open(QFile::ReadOnly)) {
+        return Student();
+    }
+    QDataStream stream(&file);
+
+    Student target;
+    stream >> target;
+
+    return target;
+}
+
+void Student::setScore(float value)
+{
+    if(value < 0) {
+        score = 0;
+
+    } else if(value > 20) {
+        score = 20;
+
+    } else {
+        score = value;
+
+    }
+}
+
+float Student::getScore() const
+{
+    return score;
+}
+
+Student::State Student::getState() const
+{
+    if(score < 10) {
+        return State::Rejection;
+
+    } else if(score < 12 && score >= 10) {
+        return State::Probation;
+
+    } else if(score < 17 && score >= 12) {
+        return State::Normal;
+
+    } else if(score < 20 && score >= 17) {
+        return State::Honor;
+
+    } else {
+        return State::Rejection;
+
+    }
+}
+
+quint8 Student::getMinimumCredits() const
+{
+    const quint8 minimum = 12;
+    return minimum;
+}
+
+quint8 Student::getMaximumCredits() const
+{
+    quint8 maximum = 0;
+    switch(getState()) {
+        case State::Probation: {
+            maximum = 14;
+            break;
+        }
+        case State::Normal: {
+            maximum = 20;
+            break;
+        }
+        case State::Honor: {
+            maximum = 24;
+            break;
+        }
+        default: {
+            maximum = getMinimumCredits();
+            break;
+        }
+    }
+    return maximum;
+}
+
+void Student::addCredit(Lesson &lesson)
+{
+    Person::addCredit(lesson);
+}
+
+void Student::removeCredit(Lesson &lesson)
+{
+    Person::removeCredit(lesson);
+}
+
+QDataStream& operator<<(QDataStream &stream, const Student &data)
+{
+    stream << static_cast<const Person&>(data);
+    stream << data.score;
+    return stream;
+}
+QDataStream& operator>>(QDataStream &stream, Student &data)
+{
+    stream >> static_cast<Person&>(data);
+    stream >> data.score;
+    return stream;
+}
