4a4a83822b2cf318cc7a9f8b85d4ca1c32b80506
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-03-30 17:28:58 +0330

    Defined Person Edit

--- include/widgets/personedit.h
+++ include/widgets/personedit.h
@@ -2,6 +2,7 @@
 #define PERSONEDIT_H
 
 #include <QWidget>
+#include "include/data/person.h"
 
 namespace Ui {
 class PersonEdit;
@@ -10,12 +11,50 @@ class PersonEdit;
 class PersonEdit : public QWidget
 {
     Q_OBJECT
-
 public:
+    explicit PersonEdit(const Person &initial, QWidget *parent = nullptr);
     explicit PersonEdit(QWidget *parent = nullptr);
     ~PersonEdit();
 
+    Person getInformation() const;
+    qint64 getIdentifier() const;
+    QString getFirstName() const;
+    QString getLastName() const;
+    QString getUserName() const;
+    QString getPassword() const;
+
+    Person getInitial() const;
+
+public slots:
+    void setFirstName(const QString &value);
+    void setLastName(const QString &value);
+    void setUserName(const QString &value);
+    void setPassword(const QString &value);
+    void setIdentifier(const qint64 &value);
+    void setInformation(const Person &value);
+
+    void setInitial(const Person &value);
+
+    void resetIdentifier();
+    void resetFirstName();
+    void resetLastName();
+    void resetUserName();
+    void resetPassword();
+
+    void resetProperties();
+
+    void toggleShowPassword(bool state);
+
+signals:
+    void firstNameChanged(const QString &value);
+    void lastNameChanged(const QString &value);
+    void userNameChanged(const QString &value);
+    void passwordChanged(const QString &value);
+    void identifierChanged(const qint64 &value);
+    void initialChanged(const Person &value);
+
 private:
+    Person initial;
     Ui::PersonEdit *ui;
 };
 
--- src/widgets/personedit.cpp
+++ src/widgets/personedit.cpp
@@ -1,12 +1,157 @@
+#include "include/widgets/core/firstnamevalidator.h"
+#include "include/widgets/core/lastnamevalidator.h"
 #include "include/widgets/personedit.h"
 #include "ui_personedit.h"
+
+PersonEdit::PersonEdit(const Person &initial, QWidget *parent) : PersonEdit(parent)
+{
+    setInitial(initial);
+}
 
 PersonEdit::PersonEdit(QWidget *parent) : QWidget(parent), ui(new Ui::PersonEdit)
 {
     ui->setupUi(this);
+    connect(ui->entityEdit, &EntityEdit::identifierAccepted, this, &PersonEdit::identifierChanged);
+    connect(ui->firstNameEdit, &QLineEdit::textChanged, this, &PersonEdit::firstNameChanged);
+    connect(ui->lastNameEdit, &QLineEdit::textChanged, this, &PersonEdit::lastNameChanged);
+    connect(ui->userNameEdit, &QLineEdit::textChanged, this, &PersonEdit::userNameChanged);
+    connect(ui->passwordEdit, &QLineEdit::textChanged, this, &PersonEdit::passwordChanged);
+    connect(this, &PersonEdit::initialChanged, this, &PersonEdit::resetProperties);
+
+    FirstNameValidator *one = new FirstNameValidator(this);
+    ui->firstNameEdit->setValidator(one);
+
+    LastNameValidator *two = new LastNameValidator(this);
+    ui->lastNameEdit->setValidator(two);
 }
 
 PersonEdit::~PersonEdit()
 {
     delete ui;
+}
+
+Person PersonEdit::getInformation() const
+{
+    Person target;
+    target.setFirstName(ui->firstNameEdit->text());
+    target.setLastName(ui->lastNameEdit->text());
+    target.setUserName(ui->userNameEdit->text());
+    target.setPassword(ui->passwordEdit->text());
+    target.setIdentifier(ui->entityEdit->getIdentifier());
+    return target;
+}
+
+QString PersonEdit::getFirstName() const
+{
+    return ui->firstNameEdit->text();
+}
+
+QString PersonEdit::getLastName() const
+{
+    return ui->lastNameEdit->text();
+}
+
+QString PersonEdit::getUserName() const
+{
+    return ui->userNameEdit->text();
+}
+
+QString PersonEdit::getPassword() const
+{
+    return ui->passwordEdit->text();
+}
+
+qint64 PersonEdit::getIdentifier() const
+{
+    return ui->entityEdit->getIdentifier();
+}
+
+Person PersonEdit::getInitial() const
+{
+    return initial;
+}
+
+void PersonEdit::setFirstName(const QString &value)
+{
+    ui->firstNameEdit->setText(value);
+}
+
+void PersonEdit::setLastName(const QString &value)
+{
+    ui->lastNameEdit->setText(value);
+}
+
+void PersonEdit::setUserName(const QString &value)
+{
+    ui->userNameEdit->setText(value);
+}
+
+void PersonEdit::setPassword(const QString &value)
+{
+    ui->passwordEdit->setText(value);
+}
+
+void PersonEdit::setIdentifier(const qint64 &value)
+{
+    ui->entityEdit->setIdentifier(value);
+}
+
+void PersonEdit::setInformation(const Person &value)
+{
+    setFirstName(value.getFirstName());
+    setLastName(value.getLastName());
+    setUserName(value.getUserName());
+    setPassword(value.getPassword());
+    setIdentifier(value.getIdentifier());
+}
+
+void PersonEdit::setInitial(const Person &value)
+{
+    this->initial = value;
+    emit initialChanged(initial);
+}
+
+void PersonEdit::resetIdentifier()
+{
+    ui->entityEdit->resetIdentifier();
+}
+
+void PersonEdit::resetFirstName()
+{
+    ui->firstNameEdit->setText(initial.getFirstName());
+}
+
+void PersonEdit::resetLastName()
+{
+    ui->lastNameEdit->setText(initial.getLastName());
+}
+
+void PersonEdit::resetUserName()
+{
+    ui->userNameEdit->setText(initial.getUserName());
+}
+
+void PersonEdit::resetPassword()
+{
+    ui->passwordEdit->setText(initial.getPassword());
+}
+
+void PersonEdit::resetProperties()
+{
+    ui->entityEdit->resetIdentifier();
+    resetFirstName();
+    resetLastName();
+    resetUserName();
+    resetPassword();
+}
+
+void PersonEdit::toggleShowPassword(bool state)
+{
+    if(state) {
+        ui->showPasswordButton->setIcon(QIcon(":/root/dialogs/password-show.png"));
+        ui->passwordEdit->setEchoMode(QLineEdit::Password);
+    } else {
+        ui->showPasswordButton->setIcon(QIcon(":/root/dialogs/password-hide.png"));
+        ui->passwordEdit->setEchoMode(QLineEdit::Normal);
+    }
 }
