2349a74f2e589569033b0ce4f8a3b2be20229cd1
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-03-31 18:13:11 +0330

    Completed Teacher Edit

--- include/widgets/teacheredit.h
+++ include/widgets/teacheredit.h
@@ -17,13 +17,59 @@ public:
     explicit TeacherEdit(QWidget *parent = nullptr);
     ~TeacherEdit();
 
+    QString getFirstName() const;
+    
+    QString getLastName() const;
+
+    QString getUserName() const;
+
+    QString getPassword() const;
+
+    qint64 getIdentifier() const;
+
     Teacher getInitial() const;
 
+    Teacher getInformation() const;
+
 public slots:
+    void setFirstName(const QString &value);
+
+    void setLastName(const QString &value);
+
+    void setUserName(const QString &value);
+
+    void setPassword(const QString &value);
+
+    void setIdentifier(qint64 value);
+
     void setInitial(const Teacher &value);
+
+    void setInformation(const Teacher &value);
+
+public slots:
+    void resetFirstName();
+
+    void resetLastName();
+
+    void resetUserName();
+
+    void resetPassword();
+
+    void resetIdentifier();
+
     void resetProperties();
 
 signals:
+    void firstNameChanged(QString value);
+
+    void lastNameChanged(QString value);
+
+    void userNameChanged(QString value);
+
+    void passwordChanged(QString value);
+
+    void identifierChanged(qint64 value);
+
     void initialChanged(const Teacher &value);
 
 private:
--- src/widgets/teacheredit.cpp
+++ src/widgets/teacheredit.cpp
@@ -10,6 +10,11 @@ TeacherEdit::TeacherEdit(QWidget *parent) : QWidget(parent), ui(new Ui::TeacherE
 {
     ui->setupUi(this);
     connect(this, &TeacherEdit::initialChanged, this, &TeacherEdit::resetProperties);
+    connect(ui->infoEditor, &PersonEdit::firstNameChanged, this, &TeacherEdit::firstNameChanged);
+    connect(ui->infoEditor, &PersonEdit::lastNameChanged, this, &TeacherEdit::lastNameChanged);
+    connect(ui->infoEditor, &PersonEdit::userNameChanged, this, &TeacherEdit::userNameChanged);
+    connect(ui->infoEditor, &PersonEdit::passwordChanged, this, &TeacherEdit::passwordChanged);
+    connect(ui->infoEditor, &PersonEdit::identifierChanged, this, &TeacherEdit::identifierChanged);
 }
 
 TeacherEdit::~TeacherEdit()
@@ -17,9 +22,66 @@ TeacherEdit::~TeacherEdit()
     delete ui;
 }
 
+QString TeacherEdit::getFirstName() const
+{
+    return ui->infoEditor->getFirstName();
+}
+
+QString TeacherEdit::getLastName() const
+{
+    return ui->infoEditor->getLastName();
+}
+
+QString TeacherEdit::getUserName() const
+{
+    return ui->infoEditor->getUserName();
+}
+
+QString TeacherEdit::getPassword() const
+{
+    return ui->infoEditor->getLastName();
+}
+
+qint64 TeacherEdit::getIdentifier() const
+{
+    return ui->infoEditor->getIdentifier();
+}
+
 Teacher TeacherEdit::getInitial() const
 {
     return initial;
+}
+
+Teacher TeacherEdit::getInformation() const
+{
+    Teacher result = initial;
+    static_cast<Person>(result) = ui->infoEditor->getInformation();
+    return result;
+}
+
+void TeacherEdit::setFirstName(const QString &value)
+{
+    ui->infoEditor->setFirstName(value);
+}
+
+void TeacherEdit::setLastName(const QString &value)
+{
+    ui->infoEditor->setLastName(value);
+}
+
+void TeacherEdit::setUserName(const QString &value)
+{
+    ui->infoEditor->setUserName(value);
+}
+
+void TeacherEdit::setPassword(const QString &value)
+{
+    ui->infoEditor->setPassword(value);
+}
+
+void TeacherEdit::setIdentifier(qint64 value)
+{
+    ui->infoEditor->setIdentifier(value);
 }
 
 void TeacherEdit::setInitial(const Teacher &value)
@@ -28,9 +90,37 @@ void TeacherEdit::setInitial(const Teacher &value)
     emit initialChanged(value);
 }
 
-void TeacherEdit::resetProperties()
+void TeacherEdit::setInformation(const Teacher &value)
 {
-    ui->infoEditor->setInformation(initial);
+    ui->infoEditor->setInformation(value);
 }
 
+void TeacherEdit::resetFirstName()
+{
+    ui->infoEditor->resetFirstName();
+}
 
+void TeacherEdit::resetLastName()
+{
+    ui->infoEditor->resetLastName();
+}
+
+void TeacherEdit::resetUserName()
+{
+    ui->infoEditor->resetUserName();
+}
+
+void TeacherEdit::resetPassword()
+{
+    ui->infoEditor->resetPassword();
+}
+
+void TeacherEdit::resetIdentifier()
+{
+    ui->infoEditor->resetIdentifier();
+}
+
+void TeacherEdit::resetProperties()
+{
+    ui->infoEditor->setInitial(initial);
+}
