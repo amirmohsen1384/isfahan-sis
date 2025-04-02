8a15cd72cf5093e83813a2ea67386a564a95b3f8
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-04-02 18:40:14 +0330

    Completed the functionality of login screen

--- include/screens/loginscreen.h
+++ include/screens/loginscreen.h
@@ -2,6 +2,8 @@
 #define LOGINSCREEN_H
 
 #include <QMainWindow>
+#include "include/data/student.h"
+#include "include/data/teacher.h"
 
 namespace Ui {
 class LoginScreen;
@@ -10,15 +12,25 @@ class LoginScreen;
 class LoginScreen : public QMainWindow
 {
     Q_OBJECT
-
 public:
+    enum class AuthorizedUser {None, Student, Teacher};
+
     explicit LoginScreen(QWidget *parent = nullptr);
     ~LoginScreen();
 
+    LoginScreen::AuthorizedUser getResult() const;
+    Teacher getAuthorizedTeacher() const;
+    Student getAuthorizedStudent() const;
+
 public slots:
+    bool authorize();
     void openAccountDialog();
+    void togglePasswordShow(bool value);
 
 private:
+    AuthorizedUser result = AuthorizedUser::None;
+    Teacher authorizedTeacher;
+    Student authorizedStudent;
     Ui::LoginScreen *ui;
 };
 
--- src/screens/loginscreen.cpp
+++ src/screens/loginscreen.cpp
@@ -1,6 +1,7 @@
 #include "include/dialogs/accountdialog.h"
 #include "include/screens/loginscreen.h"
 #include "ui_loginscreen.h"
+#include <QMessageBox>
 
 LoginScreen::LoginScreen(QWidget *parent) : QMainWindow(parent), ui(new Ui::LoginScreen)
 {
@@ -12,9 +13,78 @@ LoginScreen::~LoginScreen()
     delete ui;
 }
 
+LoginScreen::AuthorizedUser LoginScreen::getResult() const
+{
+    return result;
+}
+
+Teacher LoginScreen::getAuthorizedTeacher() const
+{
+    return authorizedTeacher;
+}
+
+Student LoginScreen::getAuthorizedStudent() const
+{
+    return authorizedStudent;
+}
+
+bool LoginScreen::authorize()
+{
+    const QString username = ui->userNameEdit->text();
+    const QString password = ui->passwordEdit->text();
+
+    QMessageBox message(this);
+    message.setIcon(QMessageBox::Critical);
+    message.setWindowTitle("Authorization Error!");
+
+    if(username.isEmpty() || password.isEmpty()) {
+        message.setText("The username or password is empty!");
+        message.exec();
+        return false;
+    }
+
+    TeacherList teachers = Teacher::getEntities();
+    for(const Teacher &target : teachers) {
+        if(target.getUserName() == username && target.getPassword() == password) {
+            result = AuthorizedUser::Teacher;
+            authorizedTeacher = target;
+            return true;
+        }
+    }
+
+    StudentList students = Student::getEntities();
+    for(const Student &target : students) {
+        if(target.getUserName() == username && target.getPassword() == password) {
+            result = AuthorizedUser::Student;
+            authorizedStudent = target;
+            return true;
+        }
+    }
+
+    message.setText("There's no user with this username or password.");
+    message.exec();
+    return false;
+}
+
 void LoginScreen::openAccountDialog()
 {
     AccountDialog dialog(this);
     dialog.setModal(true);
     dialog.exec();
+}
+
+void LoginScreen::togglePasswordShow(bool value)
+{
+    if(value)
+    {
+        ui->showPasswordButton->setIcon(QIcon(":/root/dialogs/password-hide.png"));
+        ui->passwordEdit->setEchoMode(QLineEdit::Password);
+        ui->showPasswordButton->setToolTip("Show Password");
+    }
+    else
+    {
+        ui->showPasswordButton->setIcon(QIcon(":/root/dialogs/password-show.png"));
+        ui->passwordEdit->setEchoMode(QLineEdit::Normal);
+        ui->showPasswordButton->setToolTip("Hide Password");
+    }
 }
