77674da1c07b778065a79d1f7d2b2b848e5d7f9f
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-04-03 07:59:27 +0330

    Added some features to the teacher screen

--- include/screens/teacherpanel.h
+++ include/screens/teacherpanel.h
@@ -2,6 +2,8 @@
 #define TEACHERPANEL_H
 
 #include <QMainWindow>
+#include "include/data/teacher.h"
+#include "include/models/enrolledmodel.h"
 
 namespace Ui {
 class TeacherPanel;
@@ -10,12 +12,27 @@ class TeacherPanel;
 class TeacherPanel : public QMainWindow
 {
     Q_OBJECT
-
 public:
+    explicit TeacherPanel(const Teacher &teacher, QWidget *parent = nullptr);
     explicit TeacherPanel(QWidget *parent = nullptr);
     ~TeacherPanel();
 
+    Teacher getTeacher() const;
+
+public slots:
+    void addLesson();
+
+    void setTeacher(const Teacher &teacher);
+
+public slots:
+    void resetTeacher();
+
+signals:
+    void teacherChanged(const Teacher &teacher);
+
 private:
+    Teacher teacher;
+    EnrolledModel model;
     Ui::TeacherPanel *ui;
 };
 
--- src/screens/teacherpanel.cpp
+++ src/screens/teacherpanel.cpp
@@ -1,14 +1,52 @@
-#include "teacherpanel.h"
+#include "include/dialogs/lessondialog.h"
+#include "include/screens/teacherpanel.h"
 #include "ui_teacherpanel.h"
 
-TeacherPanel::TeacherPanel(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::TeacherPanel)
+TeacherPanel::TeacherPanel(const Teacher &teacher, QWidget *parent) : TeacherPanel(parent)
+{
+    setTeacher(teacher);
+}
+
+TeacherPanel::TeacherPanel(QWidget *parent) : QMainWindow(parent), ui(new Ui::TeacherPanel)
 {
     ui->setupUi(this);
+    ui->tableView->setModel(&model);
+    ui->tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    connect(this, &TeacherPanel::teacherChanged, this, &TeacherPanel::resetTeacher);
 }
 
 TeacherPanel::~TeacherPanel()
 {
+    teacher.commitToRecord();
     delete ui;
+}
+
+Teacher TeacherPanel::getTeacher() const
+{
+    return teacher;
+}
+
+void TeacherPanel::addLesson()
+{
+    LessonDialog dialog(this);
+    if(dialog.exec() == QDialog::Accepted) {
+        Lesson target = dialog.getLesson();
+        if(!target.isNull()) {
+            teacher.addCredit(target);
+            target.commitToRecord();
+        }
+    }
+}
+
+void TeacherPanel::setTeacher(const Teacher &teacher)
+{
+    this->teacher = teacher;
+    emit teacherChanged(this->teacher);
+}
+
+void TeacherPanel::resetTeacher()
+{
+    model.setTeacher(teacher);
+
+    ui->welcomeLabel->setText(QString("Welcome back, %1!").arg(teacher.getFullName()));
 }
