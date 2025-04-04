9dc3a06187bd49b264aea89defde777969e4eea0
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-04-04 17:47:06 +0330

    Completed the Student Model

--- include/models/studentmodel.h
+++ include/models/studentmodel.h
@@ -7,15 +7,14 @@
 class StudentModel : public QAbstractTableModel
 {
     Q_OBJECT
-
 public:
     explicit StudentModel(const Student &data, QObject *parent = nullptr);
     explicit StudentModel(QObject *parent = nullptr);
 
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-
     Student& container();
 
 public slots:
--- src/models/studentmodel.cpp
+++ src/models/studentmodel.cpp
@@ -1,4 +1,12 @@
 #include "include/models/studentmodel.h"
+#include "include/data/teacher.h"
+
+#define NAME_COLUMN 0
+#define ID_COLUMN 1
+#define TEACHER_COLUMN 2
+#define BRANCH_COLUMN 3
+#define STATUS_COLUMN 4
+#define CAPACITY_COLUMN 5
 
 StudentModel::StudentModel(const Student &data, QObject *parent) : StudentModel(parent)
 {
@@ -11,6 +19,48 @@ StudentModel::StudentModel(QObject *parent) : QAbstractTableModel(parent)
     connect(&container, &Student::lessonQueueChanged, this, &StudentModel::refresh);
 }
 
+QVariant StudentModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if(role != Qt::DisplayRole) {
+        return {};
+    }
+    switch(orientation) {
+    case Qt::Horizontal: {
+        switch(section) {
+        case NAME_COLUMN: {
+            return "Name";
+        }
+        case ID_COLUMN: {
+            return "ID Number";
+        }
+        case TEACHER_COLUMN: {
+            return "Teacher";
+        }
+        case BRANCH_COLUMN: {
+            return "Branch Number";
+        }
+        case STATUS_COLUMN: {
+            return "Status";
+        }
+        case CAPACITY_COLUMN: {
+            return "Capacity";
+        }
+        default: {
+            return {};
+        }
+        }
+        break;
+    }
+    case Qt::Vertical: {
+        return section + 1;
+        break;
+    }
+    default: {
+        return {};
+    }
+    }
+}
+
 Student &StudentModel::container()
 {
     return container;
@@ -19,31 +69,100 @@ Student &StudentModel::container()
 void StudentModel::refresh()
 {
     beginResetModel();
-
     endResetModel();
-}
-
-int StudentModel::rowCount(const QModelIndex &parent) const
-{
-    if (!parent.isValid())
-        return 0;
-
-    // FIXME: Implement me!
 }
 
 int StudentModel::columnCount(const QModelIndex &parent) const
 {
-    if (!parent.isValid())
+    if (!parent.isValid()) {
         return 0;
-
-    // FIXME: Implement me!
+    }
+    return 6;
+}
+int StudentModel::rowCount(const QModelIndex &parent) const
+{
+    if (!parent.isValid()) {
+        return 0;
+    }
+    return container.getLessons().size() + container.getWaitingLessons().size();
 }
 
 QVariant StudentModel::data(const QModelIndex &index, int role) const
 {
-    if (!index.isValid())
-        return QVariant();
+    if (!index.isValid()) {
+        return {};
+    }
 
-    // FIXME: Implement me!
-    return QVariant();
+    LessonList waiting = container.getWaitingLessons();
+    LessonList resource = container.getLessons();
+
+    if(role == Qt::BackgroundRole) {
+        if(index.column() != STATUS_COLUMN) {
+            return {};
+        }
+
+        const int row = index.row();
+        if(row >= 0 && row < resource.size()) {
+            // light green
+            return QColor(200, 250, 180);
+        }
+
+        row -= resource.size();
+        if(row >= 0 && row < waiting.size()) {
+            // light red
+            return QColor(255, 160, 160);
+        }
+
+        return {};
+    }
+    else if(role != Qt::DisplayRole) {
+        return {};
+    }
+
+    Lesson target;
+    const int row = index.row();
+    if(row >= 0 && row < resource.size()) {
+        target = resource.at(row);
+        if(index.column() == STATUS_COLUMN) {
+            return "Registered";
+        }
+    }
+
+    row -= resource.size();
+    if(row >= 0 && row < waiting.size()) {
+        target = waiting.at(row);
+        if(index.column() == STATUS_COLUMN) {
+            return "In Queue";
+        }
+
+    } else {
+        return {};
+    }
+
+    switch(index.column()) {
+    case NAME_COLUMN: {
+        return target.getName();
+        break;
+    }
+    case ID_COLUMN: {
+        return target.getIdentifier();
+        break;
+    }
+    case TEACHER_COLUMN: {
+        return target.getTeacher().getFullName();
+        break;
+    }
+    case BRANCH_COLUMN: {
+        return target.getBranchNumber();
+        break;
+    }
+    case CAPACITY_COLUMN: {
+        return target.getLeftCapacity();
+        break;
+    }
+    default: {
+        return {};
+        break;
+    }
+    }
 }
