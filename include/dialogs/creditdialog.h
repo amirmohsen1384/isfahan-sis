72201123ef7144fcfc72d4794dec62e50579c241
Amir Mohsen Ghasemi <amirmohsen84ghasemi@gmail.com>
2025-04-06 15:22:30 +0330

    Defined the functionality of credit dialog

--- include/dialogs/creditdialog.h
+++ include/dialogs/creditdialog.h
@@ -4,14 +4,21 @@
 #include <QDialog>
 #include "include/data/student.h"
 
+namespace Ui { class CreditDialog; }
+
 class CreditDialog : public QDialog
 {
     Q_OBJECT
 public:
     CreditDialog(Student &initial, QWidget *parent = nullptr);
+    LessonList getLessons() const;
+
+public slots:
+    virtual void accept() override;
 
 private:
     Student *target = nullptr;
+    Ui::CreditDialog *ui = nullptr;
 };
 
 #endif // CREDITDIALOG_H
--- src/dialogs/creditdialog.cpp
+++ src/dialogs/creditdialog.cpp
@@ -1,6 +1,53 @@
 #include "include/dialogs/creditdialog.h"
+#include "include/errors/education.h"
+#include "ui_creditdialog.h"
+#include <QMessageBox>
 
-CreditDialog::CreditDialog(Student &initial, QWidget *parent) : QDialog(parent)
+CreditDialog::CreditDialog(Student &initial, QWidget *parent) : QDialog(parent), ui(new Ui::CreditDialog)
 {
+    ui->setupUi(this);
     this->target = &initial;
+}
+
+LessonList CreditDialog::getLessons() const
+{
+    return ui->container->getCredits();
+}
+
+void CreditDialog::accept()
+{
+    QMessageBox message(this);
+    LessonList initial = target->getLessons();
+    LessonList lessons = ui->container->getCredits();
+
+    try {
+        int newCredit = Lesson::getTotalCredit(lessons);
+        int totalCredit = target->getCreditCount() + newCredit;
+
+        if(target->getMinimumCredits() > totalCredit || target->getMaximumCredits() < totalCredit) {
+            throw CreditsOutOfBoundException();
+        }
+
+        for(Lesson &target : lessons) {
+            auto it = std::find_if(initial.cbegin(), initial.cend(), [&](const Lesson &element)
+            {
+                return element.getFinalExam() == target.getFinalExam();
+            });
+
+            if(it != initial.cend()) {
+                throw OverlapException(*it, target);
+            }
+
+            this->target->addCredit(target);
+        }
+
+        QDialog::accept();
+    }
+    catch(const std::exception &error) {
+        message.setWindowTitle("Error");
+        message.setIcon(QMessageBox::Critical);
+        message.setText(error.what());
+        message.exec();
+        return;
+    }
 }
